4809dd9a5550ec367719107e678f14e5fba795d4
Victor Assis <victor_assis@poli.ufrj.br>
2024-03-07 22:08:42 -0300

    feat: Heading component

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Lorem ipsum dolor sit',
+  },
+  tags: ['autodocs'],
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'Lorem ipsum dolor sit',
+    as: 'strong',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `h2`, mas podemos alterar isso com a propriedade `as`.',
+      },
+    },
+  },
+}
