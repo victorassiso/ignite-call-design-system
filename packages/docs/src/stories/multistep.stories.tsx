0f4a040727d05ded41d7e5c0eb3173b029ecb281
Victor Assis <victor_assis@poli.ufrj.br>
2024-03-11 21:11:32 -0300

    feat: Multistep

---
+++ packages/docs/src/stories/multistep.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Box, Multistep, MultistepProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Multistep',
+  component: Multistep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultistepProps>
+
+export const Primary: StoryObj<MultistepProps> = {}
---
+++ packages/react/src/components/multistep/index.tsx
@@ -0,0 +1,22 @@
+import { Label, Container, Step, Steps } from './styles'
+
+export interface MultistepProps {
+  size: number
+  currentStep?: number
+}
+
+export function Multistep({ size, currentStep = 1 }: MultistepProps) {
+  return (
+    <Container>
+      <Label>
+        Step {currentStep} of {size}
+      </Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+          return <Step key={step} active={currentStep >= step} />
+        })}
+      </Steps>
+    </Container>
+  )
+}
